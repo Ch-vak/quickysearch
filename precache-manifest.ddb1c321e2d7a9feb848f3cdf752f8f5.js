3a2ec3dbae9a37d2afc16febbfb8a086d2b90948
ChrysostomosVakasiras <61441879+ChrysostomosVakasiras@users.noreply.github.com>
2020-02-28 22:43:11 +0100

    Updates

---
+++ precache-manifest.ddb1c321e2d7a9feb848f3cdf752f8f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecefc2558476d5ffcdc06fb6df2015fb",
+    "url": "/quickysearch/index.html"
+  },
+  {
+    "revision": "82e5d37c34641b363e0c",
+    "url": "/quickysearch/static/css/main.4ff3efac.chunk.css"
+  },
+  {
+    "revision": "c66f56441c024e422d9c",
+    "url": "/quickysearch/static/js/2.5cb92c58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quickysearch/static/js/2.5cb92c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e5d37c34641b363e0c",
+    "url": "/quickysearch/static/js/main.61a1f651.chunk.js"
+  },
+  {
+    "revision": "d773c18823d7ce0e9597",
+    "url": "/quickysearch/static/js/runtime-main.8fc6e59c.js"
+  },
+  {
+    "revision": "997a5334d5062382d854f907341b19e4",
+    "url": "/quickysearch/static/media/background_search_desktop.997a5334.jpg"
+  },
+  {
+    "revision": "74c52b1bba571aa7e84e3891f5f94678",
+    "url": "/quickysearch/static/media/background_search_mobile.74c52b1b.jpg"
+  }
+]);
